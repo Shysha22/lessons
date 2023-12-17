69105b510b4eacfa48751e967ebdf7b233811361
Sasha <114261865+Shysha22@users.noreply.github.com>
2023-12-17 23:58:47 +0200

    Add files via upload

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -7,3 +7,13 @@ console.log(openMenu);
 openMenu.addEventListener("click", () => {
   header.classList.toggle("open-menu");
 });
+
+let btnHero = document.querySelectorAll(".hero__btn");
+console.log(btnHero);
+
+btnHero.forEach((btn) => {
+  btn.addEventListener("mouseover", (e) => {
+    btnHero.forEach((btn) => btn.classList.remove("hero__btn--action"));
+    e.target.classList.add("hero__btn--action");
+  });
+});
